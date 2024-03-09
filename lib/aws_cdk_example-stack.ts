1ca83dc2ff8c292151e90eb0b522cbeef96830a3
AidanLDev <dev@aidanlowson.com>
2024-03-09 17:09:51 +0000

    A short example of an AWS CDK app

--- lib/aws_cdk_example-stack.ts
+++ lib/aws_cdk_example-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
 export class AwsCdkExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, "BucketCreatedFromCDK", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
